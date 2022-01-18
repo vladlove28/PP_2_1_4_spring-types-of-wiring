daa82e7c00bd67a03cbd73c29147296b0b64db30
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:51:01 +0300

    pom update

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,7 +1,10 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
